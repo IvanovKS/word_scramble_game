4f99cf7c3c698943a9072ce85021a4c154022642
IvanovKS <Skitchi@mail.ru>
2025-03-24 00:29:31 +0300

    Add first simple components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import Input from './components/Input';
+import Button from './components/Button';
+import Timer from './components/Timer';
+import Help from './components/Help';
+import { Box, Container } from '@mui/material';
+
 function App() {
-  return <p>test</p>;
+  return (
+    <Container maxWidth="md">
+      <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+        <Help />
+        <Timer />
+        <Input />
+        <Button />
+      </Box>
+    </Container>
+  );
 }
 
 export default App;
