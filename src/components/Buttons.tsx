12116fcfebf347614a16e752e4432517557451c2
IvanovKS <Skitchi@mail.ru>
2025-03-26 22:26:03 +0300

    Add action on button "Refresh word"

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,10 +1,12 @@
-import { Button, Stack } from '@mui/material';
 import React from 'react';
+import { Button, Stack } from '@mui/material';
+import { useWord } from '../hooks/hooks';
 
 function Buttons() {
+  const { setNewWord } = useWord();
   return (
     <Stack spacing={2} direction="row">
-      <Button variant="contained" sx={{ color: 'white' }}>
+      <Button variant="contained" sx={{ color: 'white' }} onClick={setNewWord}>
         Refresh word
       </Button>
       <Button variant="contained" sx={{ color: 'white' }}>
