b58cac359d6979a7d2380775d2ce4f62502dd590
IvanovKS <Skitchi@mail.ru>
2025-04-06 22:30:59 +0300

    Add react-toastify

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,9 +1,25 @@
 import { Button, Stack } from '@mui/material';
+import { ToastContainer, toast } from 'react-toastify';
 import { useWord } from '../hooks/hooks';
 import { useTimer } from '../hooks/hooks';
 import { useInput } from '../hooks/hooks';
 
 function Buttons() {
+  const notify = (elem1: string, elem2: string): void => {
+    const property = elem1 === elem2 ? 'success' : 'error';
+    const text = elem1 === elem2 ? `You are right!` : `Try again!`;
+    toast[property](text, {
+      position: 'top-center',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+    });
+  };
+
   const { setNewWord, currentWord } = useWord();
   const { startTimer } = useTimer();
   const { inputText } = useInput();
@@ -17,6 +33,7 @@ function Buttons() {
     } else {
       console.log('no');
     }
+    notify(inputText, currentWord.word);
   };
   return (
     <Stack spacing={2} direction="row">
@@ -34,6 +51,7 @@ function Buttons() {
       >
         Check word
       </Button>
+      <ToastContainer />
     </Stack>
   );
 }
